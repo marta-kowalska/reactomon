527638aecfab37454f81cc50dad0510586071bd0
Marta Kowalska <martaaa.kowalska@gmail.com>
2021-08-19 19:28:42 +0200

    [add] picture display for each card

--- reactomon/src/Components/PokemonList.js
+++ reactomon/src/Components/PokemonList.js
@@ -8,12 +8,26 @@ export default class PokemonList extends React.Component {
     }
 
     componentDidMount(){
+        let temp;
         axios.get('https://pokeapi.co/api/v2/pokemon')
         .then(response => {
-            this.setState({ pokemons: response.data.results });
+            temp = response.data.results;
         })
-    }
+        .then(() => {
+            for(let pokemon of temp){
+                axios.get(pokemon.url)
+                .then((res) => {
+                    pokemon.image = res.data.sprites.front_default;
+                    this.setState({ pokemons: temp });
+                })
+        }})
+        }
+    
 
     render() {
-        return <div className="mini-card-wrapper">{this.state.pokemons.map(pokemon => <div className="mini-card">{pokemon.name}</div>)}</div>    }
+        return <div className="mini-card-wrapper">{this.state.pokemons.map(pokemon =>
+             <div className="mini-card">
+                 <p>{pokemon.name}</p>
+                 <p><img src={pokemon.image} alt={pokemon.name}></img></p>
+                 </div>)}</div>    }
 }
