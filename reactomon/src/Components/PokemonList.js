454baa6a8082b7cf73dd20f7304f10c3ebd41a01
Marta Kowalska <martaaa.kowalska@gmail.com>
2021-08-20 13:47:04 +0200

    [add] Card component and refactor PokemonList

--- reactomon/src/Components/PokemonList.js
+++ reactomon/src/Components/PokemonList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import '../App.css'; 
+import Card from './Card';
 
 export default class PokemonList extends React.Component {
     state = {
@@ -8,27 +9,16 @@ export default class PokemonList extends React.Component {
     }
 
     componentDidMount(){
-        let temp;
         axios.get('https://pokeapi.co/api/v2/pokemon')
         .then(response => {
-            temp = response.data.results;
+            this.setState({ pokemons: response.data.results });
         })
-        .then(() => {
-            // TODO: find another solution for image display
-            for(let pokemon of temp){
-                axios.get(pokemon.url)
-                .then((res) => {
-                    pokemon.image = res.data.sprites.front_default;
-                    this.setState({ pokemons: temp });
-                })
-        }})
         }
     
 
     render() {
-        return <div className="mini-card-wrapper">{this.state.pokemons.map(pokemon =>
-             <div className="mini-card">
-                 <p>{pokemon.name}</p>
-                 <p><img src={pokemon.image} alt={pokemon.name}></img></p>
-                 </div>)}</div>    }
+        return <div className="mini-card-wrapper">
+            {this.state.pokemons.map(pokemon =>
+                <Card item={pokemon} />)}
+                 </div>    }
 }
