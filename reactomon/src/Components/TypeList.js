a2bda03cb0fd60786d24b1c09f0f0855f13ff40f
Marta Kowalska <martaaa.kowalska@gmail.com>
2021-08-20 19:01:27 +0200

    [refactor] TypeList to use hooks

--- reactomon/src/Components/TypeList.js
+++ reactomon/src/Components/TypeList.js
@@ -1,18 +1,19 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 
-export default class TypeList extends React.Component {
-    state = {
-        types: []
-    }
+export default function TypeList() {
+    const [types, setTypes] = useState([]);
 
-    componentDidMount(){
+    useEffect(() => {
         axios.get('https://pokeapi.co/api/v2/type')
         .then(response => {
-            this.setState({ types: response.data.results });
+            setTypes(response.data.results);
         })
-    }
+    })
 
-    render() {
-        return <div className="mini-card-wrapper">{this.state.types.map(type => <div className="mini-card">{type.name}</div>)}</div>    }
+    return (
+        <div className="mini-card-wrapper">
+            {types.map(type => <div className="mini-card">{type.name}</div>)}
+        </div>
+    )
 }
