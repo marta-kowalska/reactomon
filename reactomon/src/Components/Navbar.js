2e270b1a8366ee8f5d55f78ef8e27321a32e7178
Marta Kowalska <martaaa.kowalska@gmail.com>
2021-08-20 18:52:18 +0200

    [refactor] Navbar

--- reactomon/src/Components/Navbar.js
+++ reactomon/src/Components/Navbar.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 
-export default class Navbar extends React.Component {
+export default function Navbar() {
 
-    render() {
         return (<nav>
         <Link to="/pokemons" className="App-linkButton">Pokemons</Link>
         <Link to="/types" className="App-linkButton">Types</Link>         
     </nav>)   
-    }
 }
