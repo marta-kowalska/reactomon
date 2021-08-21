58d595bcdc418bcfe43acde9c89b532b629aaab9
Marta Kowalska <martaaa.kowalska@gmail.com>
2021-08-21 23:24:23 +0200

    [change] Navbar styling to styled component

--- reactomon/src/Components/Navbar.js
+++ reactomon/src/Components/Navbar.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import styled from 'styled-components';
 import { Link } from "react-router-dom";
+
+const LinkButton = styled.button`
+    text-decoration: none;
+    padding: 10px;
+    margin: 20px;
+    background-color: goldenrod;
+    border: none;
+    &:visited{
+        color: black;
+    }
+    &:hover{
+        background-color:black;
+  	color: goldenrod;
+    }
+`
 
 export default function Navbar() {
 
         return (<nav>
-        <Link to="/pokemons" className="App-linkButton">Pokemons</Link>
-        <Link to="/types" className="App-linkButton">Types</Link>         
+        <Link to="/pokemons"><LinkButton>Pokemons</LinkButton></Link>
+        <Link to="/types"><LinkButton>Types</LinkButton></Link>         
     </nav>)   
 }
